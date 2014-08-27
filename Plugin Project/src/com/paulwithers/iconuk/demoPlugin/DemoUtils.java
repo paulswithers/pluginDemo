3156a3b1d199ae98b9cc83bc85e482d7fdba1543
Paul Withers <paulswithers@hotmail.co.uk>
2014-08-27 13:44:17 +0100

    Adding utility method to convert any Java object to a String detailing all properties via reflection

--- Plugin Project/src/com/paulwithers/iconuk/demoPlugin/DemoUtils.java
+++ Plugin Project/src/com/paulwithers/iconuk/demoPlugin/DemoUtils.java
@@ -1,5 +1,27 @@
 package com.paulwithers.iconuk.demoPlugin;
 
+import java.security.AccessControlException;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
 public class DemoUtils {
 
+	public static String convertObjectToString(final Object o) {
+		String retVal_ = "";
+		try {
+			retVal_ = AccessController.doPrivileged(new PrivilegedExceptionAction<String>() {
+				public String run() throws Exception {
+					return ReflectionToStringBuilder.toString(o);
+				}
+			});
+		} catch (AccessControlException e) {
+			e.printStackTrace();
+		} catch (PrivilegedActionException e) {
+			e.printStackTrace();
+		}
+		return retVal_;
+	}
 }
